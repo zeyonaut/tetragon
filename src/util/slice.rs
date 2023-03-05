fa1ab761447c8cc93784a9ea4cf01fe7e25af946
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2023-03-05 19:38:43 +0000

    Use match in match_slice

--- src/util/slice.rs
+++ src/util/slice.rs
@@ -32,41 +32,39 @@ macro_rules! match_slice {
 		let slice = $slice;
 		// REASON: This is necessary to prevent linting for unused variables in the outer matching step, as they only get used when reintroduced in the inner step.
 		#[allow(unused_variables)]
-		if false {
-			// SAFETY: The body of an if expression with a false condition can never run.
-			unsafe { ::core::hint::unreachable_unchecked() }
-		}
-		$(
-			else if let [$($pattern),*] = slice.as_ref() {
-				let _slice = transmute_to_slice_of_maybe_uninit(slice);
-				#[allow(unused_variables, unused_mut)]
-				let mut counter = 0;
-				#[allow(unused_assignments)]
-				let scrutinee =
-				(
-					$(
-						{
-							match_slice![@ignore $pattern];
-							// SAFETY: This element was confirmed to exist and be initialized by the outer matching step.
-							let x = unsafe {_slice.get_unchecked(counter).assume_init_read()};
-							counter += 1;
-							x
-						},
-					)*
-				);
-				// REASON: This is necessary to prevent linting for the unreachable pattern we introduce below.
-				#[allow(unreachable_patterns)]
-				match scrutinee {
-					($($pattern,)*) => {
-						$body
+		match slice.as_ref() {
+			$(
+				[$($pattern),*] => {
+					let _slice = transmute_to_slice_of_maybe_uninit(slice);
+					#[allow(unused_variables, unused_mut)]
+					let mut counter = 0;
+					#[allow(unused_assignments)]
+					let scrutinee =
+					(
+						$(
+							{
+								match_slice![@ignore $pattern];
+								// SAFETY: This element was confirmed to exist and be initialized by the outer matching step.
+								let x = unsafe {_slice.get_unchecked(counter).assume_init_read()};
+								counter += 1;
+								x
+							},
+						)*
+					);
+					// REASON: This is necessary to prevent linting for the unreachable pattern we introduce below.
+					#[allow(unreachable_patterns)]
+					match scrutinee {
+						($($pattern,)*) => {
+							$body
+						}
+						// SAFETY: This scrutinee was confirmed to match the pattern by the outer matching step.
+						_ => unsafe { ::core::hint::unreachable_unchecked() }
 					}
-					// SAFETY: This scrutinee was confirmed to match the pattern by the outer matching step.
-					_ => unsafe { ::core::hint::unreachable_unchecked() }
-				}
+				},
+			)*
+			_ => {
+				$fallback_body
 			}
-		)*
-		else {
-			$fallback_body
 		}
 	};
 }
