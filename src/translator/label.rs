4fe6064facc5b142170e10651eca8fa99891f069
Aaron Yeoh Cruz <zeyonaut@gmail.com>
2023-04-10 21:58:04 +0100

    Fix generation of and add accessor for labels

--- src/translator/label.rs
+++ src/translator/label.rs
@@ -4,13 +4,19 @@ pub struct LabelGenerator(u64);
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct Label(u64);
 
+impl Label {
+	pub fn handle(&self) -> u64 {
+		self.0
+	}
+}
+
 impl LabelGenerator {
 	pub fn new() -> Self {
 		Self(0)
 	}
 
 	pub fn fresh<const N: usize>(&mut self) -> [Label; N] {
-		[0; N].map(|_| {
+		[(); N].map(|()| {
 			let symbol = self.0;
 			self.0 += 1;
 			Label(symbol)
